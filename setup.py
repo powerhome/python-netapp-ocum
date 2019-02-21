c3774da3206b92e0ad785591ff4ea5604296958a
David Taylor <djtaylor13@gmail.com>
2019-02-21 12:33:13 -0500

    Rename to netapp_ocum

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@
 from setuptools import setup, find_packages
 
 # Import the module version
-from netapp_uom import __version__
+from netapp_ocum import __version__
 
 # Run the setup
 setup(
-    name             = 'netapp_uom',
+    name             = 'netapp_ocum',
     version          = __version__,
-    description      = 'Python bindings for interacting with the NetApp UOM.',
+    description      = 'Client bindings for for the NetApp OnCommand Unified Manager (OCUM) REST API.',
     long_description = open('DESCRIPTION.rst').read(),
     author           = 'David Taylor',
     author_email     = 'djtaylor13@gmail.com',
-    url              = 'http://github.com/djtaylor/python-netapp-uom',
+    url              = 'http://github.com/djtaylor/python-netapp-ocum',
     license          = 'GPLv3',
     test_suite       = 'nose.collector',
     tests_require    = ['nose'],
     packages         = find_packages(),
-    keywords         = 'netapp nfs api http',
+    keywords         = 'netapp nfs api http ocum',
     classifiers      = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
