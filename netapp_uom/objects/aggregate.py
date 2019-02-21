99e76ad0ad8a6370952a98de37a09461769055e5
David Taylor <djtaylor13@gmail.com>
2019-02-21 12:33:52 -0500

    Delete old netapp_uom folder

--- netapp_uom/objects/aggregate.py
+++
@@ -1,8 +0,0 @@
-from netapp_uom.objects.base import NetApp_UOM_ObjectBase
-
-class NetApp_UOM_Aggregate(NetApp_UOM_ObjectBase):
-    """
-    Class representing a single NetApp aggregate.
-    """
-    def __init__(self, object_json):
-        super().__init__(object_json)
--- netapp_uom/objects/cluster.py
+++
@@ -1,8 +0,0 @@
-from netapp_uom.objects.base import NetApp_UOM_ObjectBase
-
-class NetApp_UOM_Cluster(NetApp_UOM_ObjectBase):
-    """
-    Class representing a single NetApp cluster.
-    """
-    def __init__(self, object_json):
-        super().__init__(object_json)
